80d63cd3e200506c17d5b6b099355a389cef63e1
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-04 22:46:12 -0300

    Getting all necessary html elements and storing in consts.

---
+++ pokedex/app.js
@@ -0,0 +1,18 @@
+// EXTRACT REQUIRED DOM ELEMENTS
+const pokeName = document.querySelector(".poke-name");
+const pokeId = document.querySelector(".poke-id");
+const pokeFrontImage = document.querySelector(".poke-front-image");
+const pokeBackImage = document.querySelector(".poke-back-image");
+const pokeTypeOne = document.querySelector(".poke-type-one");
+const pokeTypeTwo = document.querySelector(".poke-type-two")
+const pokeWeight = document.querySelector(".poke-weight");
+const pokeHeight = document.querySelector(".poke-height");
+
+console.log(pokeName);
+
+
+fetch ("https://pokeapi.co/api/v2/pokemon/1")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
