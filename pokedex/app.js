1e047927f1d70a8f7739f47835459a687b123c89
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-06 14:08:54 -0300

    Displaying the pokémon info on the screen.

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,3 +1,5 @@
+const mainScreen = document.querySelector(".main-screen");
+
 // EXTRACT REQUIRED DOM ELEMENTS
 const pokeName = document.querySelector(".poke-name");
 const pokeId = document.querySelector(".poke-id");
@@ -11,8 +13,31 @@ const pokeHeight = document.querySelector(".poke-height");
 console.log(pokeName);
 
 
-fetch ("https://pokeapi.co/api/v2/pokemon/1")
+fetch ("https://pokeapi.co/api/v2/pokemon/4")
     .then(res => res.json())
     .then(data => {
         console.log(data);
+
+        mainScreen.classList.remove("hide");
+        pokeName.textContent = data["name"];
+        pokeId.textContent = data["id"];
+        pokeWeight.textContent = data["weight"];
+        pokeHeight.textContent = data["height"];
+
+        const pokeTypes = data["types"];
+        const firstType = pokeTypes[0];
+        const secondType = pokeTypes[1];
+
+        pokeTypeOne.textContent = firstType["type"]["name"];
+
+        if (secondType) {
+            pokeTypeTwo.style.display = "block";
+            pokeTypeTwo.textContent = secondType["type"]["name"];
+        } else {
+            pokeTypeOne.style.margin = "25px 0px 0px 0px";
+            pokeTypeTwo.style.display = "none";
+        }
+
+        pokeFrontImage.src = data["sprites"]["front_default"] || "";
+        pokeBackImage.src = data["sprites"]["back_default"] || "";
     });
