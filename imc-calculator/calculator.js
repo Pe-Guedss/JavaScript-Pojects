6ddc834774106c60fd290f0e877c327f6f4031f1
Pe-Guedss <pedro.og2002@gmail.com>
2022-08-22 23:27:36 -0300

    Feat: Highlighting the IMC category after calculating it.

--- imc-calculator/calculator.js
+++ imc-calculator/calculator.js
@@ -4,7 +4,25 @@ function calculate(ev) {
     let weight = document.getElementById("weight").value;
     let result = document.getElementById("result");
 
-    let imc = weight/height^2;
+    let imc = weight*1.0/(height^2);
 
-    result.innerHTML = `Seu IMC é: ${imc}. Observe a seção das categorias acima!`
+    result.innerHTML = `Seu IMC é: ${imc}. Observe a seção das categorias acima!`;
+
+    let category;
+    if (imc < 17) {
+        category = document.getElementById("very-below");
+    } else if (imc >= 17 && imc < 18.5) {
+        category = document.getElementById("below");
+    }
+    else if (imc >= 18.5 && imc < 25) {
+        category = document.getElementById("ideal");
+    }
+    else if (imc >= 25 && imc < 30) {
+        category = document.getElementById("above");
+    }
+    else {
+        category = document.getElementById("very-above");
+    }
+
+    category.style.color = "red";
 }
